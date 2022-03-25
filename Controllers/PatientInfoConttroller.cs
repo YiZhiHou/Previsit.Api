cc936dd8dab620dc97b4ea79e1bba4db9f782a06
yinzihao <2420188520@qq.com>
2022-03-25 11:07:55 +0800

    添加项目文件。

---
+++ Controllers/PatientInfoConttroller.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Previsit.Api.Controllers
+{
+    [Route("api/patientinformation")]
+    [ApiController]
+    public class PatientInfoConttroller : Controller
+    {
+        /// <summary>
+        /// 更改患者信息
+        /// </summary>
+        /// <param name="patientinfo"></param>
+        /// <returns></returns>
+        [HttpPost("updatepatientinfo")]
+        public async Task<bool> UpdatePatientInformation(string patientinfo)
+        {
+            
+            return true;
+        }
+
+        /// <summary>
+        /// 获取患者信息
+        /// </summary>
+        /// <param name="patientid"></param>
+        /// <returns></returns>
+        [HttpGet("getpatientinfo")]
+        public async Task<bool> GetPatientInformation(string patientid)
+        {
+            
+            return true;
+        }
+    }
+}
