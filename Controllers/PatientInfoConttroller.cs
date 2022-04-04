cdf9d85b2b32994dfa4f49e525cf817336667ad3
yinzihao <2420188520@qq.com>
2022-04-04 18:13:33 +0800

    patientInfo complete

--- Controllers/PatientInfoConttroller.cs
+++ Controllers/PatientInfoConttroller.cs
@@ -2,36 +2,53 @@
 
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Previsit.Api.Bll.Conmon;
+using Previsit.Api.Bll.Interface;
+using Previsit.Api.Model.Conmon;
+using Previsit.Api.Model.Models;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Previsit.Api.Controllers
 {
-    [Route("api/patientinformation")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PatientInfoConttroller : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly IPatientInfoBll _patientInfoBll;
+        public PatientInfoConttroller(IConfiguration configuration, IPatientInfoBll patientInfoBll)
+        {
+            _configuration = configuration;
+            _patientInfoBll = patientInfoBll;
+        }
+
+
         /// <summary>
         /// 更改患者信息
         /// </summary>
         /// <param name="patientinfo"></param>
         /// <returns></returns>
         [HttpPost("updatepatientinfo")]
-        public async Task<bool> UpdatePatientInformation(string patientinfo)
+        public async Task<ResultModel> UpdatePatientInformation(PatientInfo patientinfo)
         {
-            
-            return true;
+            var result = await _patientInfoBll.UpdatePatientInfoBll(patientinfo);
+            //_logger.LogInformation($"更改患者信息，患者Id为:{patientinfo.PatientId}");
+            return PackResultModel.PackResult(result, HttpStatusCode.OK, "信息更新成功");
         }
 
         /// <summary>
         /// 获取患者信息
         /// </summary>
-        /// <param name="patientid"></param>
+        /// <param name="visit_card_id"></param>
         /// <returns></returns>
         [HttpGet("getpatientinfo")]
-        public async Task<bool> GetPatientInformation(string patientid)
+        public async Task<ResultModel> GetPatientInformation(int visit_card_id)
         {
-            
-            return true;
+            var result = await _patientInfoBll.GetPatientInfoByVisitCardIdBll(visit_card_id);
+            //_logger.LogInformation($"获取患者信息，患者Id为:{visit_card_id}");
+            return PackResultModel.PackResult(result, HttpStatusCode.OK, "信息获取成功");
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Previsit.Api.Bll;
+using Previsit.Api.Bll.Interface;
+using Previsit.Api.Dal.Repository;
+using Previsit.Api.Dal.Repository.Interface;
 using Prometheus;
 using Swashbuckle.AspNetCore.Filters;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
+using Previsit.Api.Dal.PreDbContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace Previsit.Api
 {
@@ -72,15 +72,16 @@ namespace Previsit.Api
             #endregion Swagger
 
 
-
+            services.AddDbContext<PrevisitDbContext>(option =>
+            {
+                option.UseNpgsql(Configuration["Db:ExampleDb:ConnectionString"]);
+            });
             services.AddSingleton(Configuration);
-        //    services.AddDbContext<PrevisitDbContext>(option =>
-        //    {
-        //        option.UseNpgsql(Configuration["DBCONNECTION_CQMWECHAT"]);
-         //   });
-
 
             //依赖注入
+            //services.AddScoped<IPatientInfoBll, PatientInfoBll>();
+            services.AddScoped<IPatientInfoRepository, PatientInfoRepository>();
+            services.AddScoped<IPatientInfoBll, PatientInfoBll>();
 
         }
 
@@ -91,6 +92,22 @@ namespace Previsit.Api
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Previsit API V1");
+            });
+
+            app.UseHttpMetrics();
+            app.UseMetricServer();
+
+            app.UseCors("default");
+
+            app.UseRouting();
+
+            //  请放置在 routing和Endpoints之间
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapMetrics();
+                endpoints.MapControllers();
             });
 
             app.UseStaticFiles();
